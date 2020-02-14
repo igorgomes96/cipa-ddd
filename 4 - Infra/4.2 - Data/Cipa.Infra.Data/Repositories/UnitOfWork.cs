bf3557d9c928baee41a9fa00b638658387e799d6
Igor Gomes <igorgomes96@hotmail.com>
2020-02-14 09:17:58 -0300

    Ajuste Lint

--- 4 - Infra/4.2 - Data/Cipa.Infra.Data/Repositories/UnitOfWork.cs
+++ 4 - Infra/4.2 - Data/Cipa.Infra.Data/Repositories/UnitOfWork.cs
@@ -26,7 +26,7 @@ namespace Cipa.Infra.Data.Repositories
                     Context.SaveChanges();
                     dbContextTransaction.Commit();
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     //Log Exception Handling message                      
                     dbContextTransaction.Rollback();
