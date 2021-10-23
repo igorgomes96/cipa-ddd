60217151438c13393061fa8ff6d42e945fce6646
Igor Gomes <igorgomes96@hotmail.com>
2021-10-23 19:32:01 -0300

    Adicionando log

--- 4 - Infra/Cipa.Infra.Data/Utils/EmailSender.cs
+++ 4 - Infra/Cipa.Infra.Data/Utils/EmailSender.cs
@@ -8,13 +8,15 @@ using Cipa.Domain.Helpers;
 using Cipa.Domain.Services.Interfaces;
 using Microsoft.Extensions.Logging;
 
-namespace Cipa.Infra.Data.Utils {
+namespace Cipa.Infra.Data.Utils
+{
     public class EmailSender : IEmailSender
     {
         private IAmazonSimpleEmailService ses;
         private EmailConfiguration emailConfiguration;
         private ILogger<EmailSender> logger;
-        public EmailSender(IAmazonSimpleEmailService ses, EmailConfiguration emailConfiguration, ILogger<EmailSender> logger) {
+        public EmailSender(IAmazonSimpleEmailService ses, EmailConfiguration emailConfiguration, ILogger<EmailSender> logger)
+        {
             this.ses = ses;
             this.emailConfiguration = emailConfiguration;
             this.logger = logger;
@@ -22,41 +24,41 @@ namespace Cipa.Infra.Data.Utils {
         public async Task Send(Email email)
         {
             var sendRequest = new SendEmailRequest
+            {
+                Source = emailConfiguration.Alias,
+                SourceArn = emailConfiguration.SESArn,
+                Destination = new Destination
                 {
-                    Source = emailConfiguration.Alias,
-                    SourceArn = emailConfiguration.SESArn,
-                    Destination = new Destination
+                    ToAddresses = email.DestinatariosLista.ToList(),
+                    CcAddresses = email.CopiasLista.ToList()
+                },
+                Message = new Message
+                {
+                    Subject = new Content(email.Assunto),
+                    Body = new Body
                     {
-                        ToAddresses = email.DestinatariosLista.ToList(),
-                        CcAddresses = email.CopiasLista.ToList()
-                    },
-                    Message = new Message
-                    {
-                        Subject = new Content(email.Assunto),
-                        Body = new Body
+                        Html = new Content
                         {
-                            Html = new Content
-                            {
-                                Charset = "UTF-8",
-                                Data = email.MensagemEstilizada
-                            }
+                            Charset = "UTF-8",
+                            Data = email.MensagemEstilizada
                         }
                     }
-                };
+                }
+            };
 
-                try
-                {
-                    logger.LogInformation("Enviando email pelo SES: {0}, para [{1}]", email.Assunto, email.Destinatarios);
-                    var response = await ses.SendEmailAsync(sendRequest);
-                    email.StatusEnvio = StatusEnvio.EnviadoComSucesso;
-                    email.MensagemErro = null;
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Erro ao enviar email.");
-                    email.StatusEnvio = StatusEnvio.FalhaNoEnvio;
-                    email.MensagemErro = ex.ToString();
-                }
+            try
+            {
+                logger.LogInformation("Enviando email pelo SES: {0}, para [{1}]. Identifier ARN: {2}", sendRequest.Source, email.Destinatarios, sendRequest.SourceArn);
+                var response = await ses.SendEmailAsync(sendRequest);
+                email.StatusEnvio = StatusEnvio.EnviadoComSucesso;
+                email.MensagemErro = null;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Erro ao enviar email.");
+                email.StatusEnvio = StatusEnvio.FalhaNoEnvio;
+                email.MensagemErro = ex.ToString();
+            }
         }
     }
 }
