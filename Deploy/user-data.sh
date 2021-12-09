714629e6cee202ba5672679c9e88e8d9739cfc0d
Igor Gomes <igorgomes96@hotmail.com>
2021-12-09 16:57:38 -0300

    Criando diretorio StaticFiles

--- Deploy/user-data.sh
+++ Deploy/user-data.sh
@@ -7,5 +7,7 @@ cd "/src/cipa-api/1 - WebApi/Cipa.WebApi"
 export DOTNET_CLI_HOME=/tmp
 /usr/bin/dotnet publish --configuration Release /p:CustomTransformFileName=aws.transform -o /var/www/cipa
 chmod -R 777 /var/www/cipa/Assets
+mkdir /var/www/cipa/StaticFiles
+chmod -R 777 /var/www/cipa/StaticFiles
 systemctl restart cipa
 systemctl restart nginx
