34ac72d1cde60601be6fd91e246ab897d32353ff
Igor Gomes <igorgomes96@hotmail.com>
2023-11-25 15:51:43 -0300

    Getting real ip from request

--- 1 - WebApi/Cipa.WebApi/Controllers/Controller.cs
+++ 1 - WebApi/Cipa.WebApi/Controllers/Controller.cs
@@ -6,48 +6,29 @@ namespace Cipa.WebApi.Controllers
 {
     public class Controller : ControllerBase
     {
-        protected string IpRequisicao
-        {
-            get
-            {
-                return Request.HttpContext.Connection.RemoteIpAddress.ToString();
-            }
-        }
+        protected string IpRequisicao => 
+            Request.Headers.TryGetValue("X-Forwarded-For", out var ip) ?
+                ip.First() :
+                Request.HttpContext.Connection.RemoteIpAddress!.ToString();
 
         protected int ContaId
         {
-            get
-            {
-                return int.Parse(User.Claims.First(c => c.Type == CustomClaimTypes.CodigoConta).Value);
-            }
+            get { return int.Parse(User.Claims.First(c => c.Type == CustomClaimTypes.CodigoConta).Value); }
         }
 
         protected int UsuarioId
         {
-            get
-            {
-                return int.Parse(User.Claims.First(c => c.Type == CustomClaimTypes.UsuarioId).Value);
-            }
+            get { return int.Parse(User.Claims.First(c => c.Type == CustomClaimTypes.UsuarioId).Value); }
         }
 
         protected string NomeUsuario
         {
-            get
-            {
-                return User.Claims.First(c => c.Type == CustomClaimTypes.NomeUsuario).Value;
-            }
+            get { return User.Claims.First(c => c.Type == CustomClaimTypes.NomeUsuario).Value; }
         }
 
-        protected string EmailUsuario
-        {
-            get
-            {
-                return User.Identity.Name;
-            }
-        }
+        protected string EmailUsuario => User.Identity.Name;
 
         protected bool UsuarioEhDoSESMT => User.IsInRole(PerfilUsuario.SESMT);
         protected bool UsuarioEhAdministrador => User.IsInRole(PerfilUsuario.Administrador);
-
     }
 }
